6bc153b90dd758d82858b74ab5ec51827f7a4716
cyara <cmyarap@gmail.com>
2023-11-29 23:25:15 -0500

    Se ajustan servicios entre otros

--- src/app/helpers/services/distributor.service.ts
+++ src/app/helpers/services/distributor.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment as env } from '../../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TokenService } from './token.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,22 +17,22 @@ export class DistributorService {
       Authorization: `Basic ${this.tokenService.getToken()}`,
     });}
 
-  getDistributors(){
+  getDistributors():Observable<any>{
     return this.http.get(`${this.apiUrl}/distributors/`, { headers: this.headers })
   }
 
-  postDistributors(id:string, data:any){
+  postDistributors(id:string, data:any):Observable<any>{
     return this.http.post(`${this.apiUrl}/distributors/${id}`, data, { headers: this.headers })
   }
-  getDistributorDetails(id:string){
+  getDistributorDetails(id:string):Observable<any>{
     return this.http.get(`${this.apiUrl}/distributor/${id}`, { headers: this.headers })
   }
 
-  updateDistributor( payload:any,id:string){
+  updateDistributor( payload:any,id:string):Observable<any>{
     return this.http.patch(`${this.apiUrl}/distributor/${id}`, payload, { headers: this.headers })
   }
 
-  deleteDistributor(id:string){
+  deleteDistributor(id:string):Observable<any>{
     return this.http.delete(`${this.apiUrl}/distributor/${id }`, { headers: this.headers })
   }
 }
--- src/app/helpers/services/tickets.service.ts
+++ src/app/helpers/services/tickets.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment as env } from '../../../environments/environment';
 import { StorageService } from './storage.service';
 import { TokenService } from './token.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -18,24 +19,24 @@ export class TicketsService {
     });
   }
 
-  createTicket(data:any){
+  createTicket(data:any):Observable<any>{
     let id = parseInt(this.storageService.getUserID())
     return this.http.post(`${this.apiUrl}/tickets/`, Object.assign(data, {user_id: id, user_it_id : null}), { headers: this.headers })
   }
 
-  getTickets(){
+  getTickets():Observable<any>{
     return this.http.get(`${this.apiUrl}/tickets/`, { headers: this.headers })
   }
 
-  getTicket(id:any){
+  getTicket(id:any):Observable<any>{
     return this.http.get(`${this.apiUrl}/ticket/${id}`, { headers: this.headers })
   }
 
-  updateTicket(id:any, payload:any){
+  updateTicket(id:any, payload:any):Observable<any>{
     return this.http.put(`${this.apiUrl}/ticket/${id}`, payload, { headers: this.headers })
   }
 
-  deleteTicket(id:any){
+  deleteTicket(id:any):Observable<any>{
     return this.http.delete(`${this.apiUrl}/ticket/${id}`, { headers: this.headers })
   }
 }
